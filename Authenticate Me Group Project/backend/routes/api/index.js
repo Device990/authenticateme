baa41136305955547a632b16450fa4d7633655e5
device990 <brialon224@gmail.com>
2024-12-13 13:23:38 -0800

    Starting Phase 4

--- Authenticate Me Group Project/backend/routes/api/index.js
+++ Authenticate Me Group Project/backend/routes/api/index.js
@@ -49,28 +49,20 @@ router.get(
 module.exports = router;
 // ...
 
-
-
-
 */
 
 
-
-
-
-// backend/routes/api/index.js
 const router = require("express").Router();
 const { restoreUser } = require("../../utils/auth.js");
 
-
-
-
-router.get('/helloworld', (req, res) => {
-  res.json({ "hello": "world"} )
-})
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 router.use(restoreUser);
 
 module.exports = router;
+
+
+
+
+
